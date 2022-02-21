29440f80a013e786cefc5ff703021c88159e98f3
zakiya adam <zakiyaadan12@gmail.com>
2022-02-21 14:46:57 +0300

    test if classes are being initialized properly

--- Password-Locker/password.py
+++ Password-Locker/password.py
@@ -1,11 +1,12 @@
 class User:
-  '''
-  Class that generates new instances of users
+   '''
+   Class that generates new instances of users
+   '''
 
-  user_list = [] # Empty user list
+   user_list = [] # Empty user list
  
-  def __init__(self,username,password):
-    '''this method helps us define properties of a user'''
+   def __init__(self,username,password):
+      '''method that defines properties of a user'''
 
-    self.username = username
-    self.password = password
+      self.username = username
+      self.password = password
--- Password-Locker/password_test.py
+++ Password-Locker/password_test.py
@@ -2,3 +2,22 @@ import unittest # Importing the unittest module
 from password import User # Importing the password class
 
 class TestUser(unittest.TestCase):
+
+    def setUp(self):
+      '''
+      Set up method to run before each test cases.
+      '''
+      self.new_user = User("Bintadam","password") # create contact object
+
+
+    def test_init(self):
+      '''
+      test_init test case to test if the class has been initialized properly
+      '''
+
+      self.assertEqual(self.new_user.username, "Bintadam")
+      self.assertEqual(self.new_user.password,"password")
+        
+
+if __name__ == '__main__':
+    unittest.main()
