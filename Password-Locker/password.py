d7378415ec4e02788ebc769d1e88bf81ab6ae67f
zakiya adam <zakiyaadan12@gmail.com>
2022-02-21 14:09:00 +0300

    created instance variables

--- Password-Locker/password.py
+++ Password-Locker/password.py
@@ -1,4 +1,10 @@
- class Contact:
-    """
-    Class that generates new instances of contacts
-    """
+class user:
+   """
+   Class that generates new instances of users
+   """
+    def __init__(self,username,password):
+        '''
+        __init__ method that helps us define properties of a user.
+
+        self.username = username
+        self.password = password
